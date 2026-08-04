c17efefc7da715fc517943b4af983aec386ed7d7
SomeoneThatYouKnow <Insults3@gmail.com>
2026-08-05 01:34:30 +0300

    Update a8Fh2k.user.js

--- a8Fh2k.user.js
+++ a8Fh2k.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Made By Lukas
 // @namespace    core.lukas
-// @version      1.8
+// @version      1.9
 // @match        https://*.tankionline.com/play/*
 // @match        https://*.tankionline.com/browser-public/*
 // @run-at       document-start
